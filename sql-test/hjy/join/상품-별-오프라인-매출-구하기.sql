f3282687de06be8f35dae6a3dbdbad7ad7193650
Xuswnss <cooconut0226@gmail.com>
2024-10-17 19:43:41 +0900

    20241017hjy

--- sql-test/hjy/join/상품-별-오프라인-매출-구하기.sql
+++ sql-test/hjy/join/상품-별-오프라인-매출-구하기.sql
@@ -1,1 +1,10 @@
-hello
+-- 코드를 입력하세요
+# SELECT p.product_code,
+         # from product p  join offline_sale s
+      # on p.product_id = s.product_id
+
+select  p.product_code, sum(s.sales_amount)*p.price sales
+from product p join offline_sale s
+                    on p.product_id = s.product_id
+group by p.product_id
+order by 2 desc, 1
