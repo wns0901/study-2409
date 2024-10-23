23f85d7b1def12b7d6e20432c07602297c9409a3
jjy <wnswns012@gmai.com>
2024-10-23 20:17:00 +0900

    폴더구조 변경[jjy]

--- coding-test/src/jjy/프로그래머스/day241022/x만큼_간격이_있는_n개의_숫자/Solution.java
+++ coding-test/src/jjy/프로그래머스/day241022/x만큼_간격이_있는_n개의_숫자/Solution.java
@@ -1,4 +1,4 @@
-package jjy.프로그래머스.x만큼_간격이_있는_n개의_숫자;
+package jjy.프로그래머스.day241022.x만큼_간격이_있는_n개의_숫자;
 
 class Solution {
     public long[] solution(int x, int n) {
--- coding-test/src/jjy/프로그래머스/day241022/약수의합/Solution.java
+++ coding-test/src/jjy/프로그래머스/day241022/약수의합/Solution.java
@@ -1,4 +1,4 @@
-package jjy.프로그래머스.약수의합;
+package jjy.프로그래머스.day241022.약수의합;
 
 public class Solution {
     public int solution(int n) {
