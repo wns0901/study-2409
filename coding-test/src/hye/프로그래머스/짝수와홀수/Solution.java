21ac7459c070768ad7410fb3dc1d2c9179c3c96d
solsol <le_fox.sol@hwang-yeeun-ui-MacBookAir.local>
2024-10-23 14:59:53 +0900

    짝수와홀수문제완료[hye]

---
+++ coding-test/src/hye/프로그래머스/짝수와홀수/Solution.java
@@ -0,0 +1,13 @@
+package hye.프로그래머스.짝수와홀수;
+
+public class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if (num % 2 == 0){
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
---
+++ coding-test/src/hye/프로그래머스/평균_구하기/Solution.java
@@ -0,0 +1,5 @@
+package hye.프로그래머스.평균_구하기;
+
+public class Solution {
+
+}
