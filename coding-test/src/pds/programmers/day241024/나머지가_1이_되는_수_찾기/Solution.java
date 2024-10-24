6acf823b63ed49a29ba00e6a1e4fec6953c80836
Parkdongshin <rktks789@naver.com>
2024-10-24 17:01:25 +0900

    ãprogrammers 2problem[pds]

---
+++ coding-test/src/pds/programmers/day241024/나머지가_1이_되는_수_찾기/Solution.java
@@ -0,0 +1,17 @@
+package pds.programmers.day241024.나머지가_1이_되는_수_찾기;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 2; i < n; i++){
+            if(n % i == 1){
+                answer = i;
+                return answer;
+            }
+
+        }
+        return -1;
+    }
+
+}
