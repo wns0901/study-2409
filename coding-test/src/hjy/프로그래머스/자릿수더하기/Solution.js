cbc0b17404429b424846ee62e7e2054c1a47ec5d
Xuswnss <cooconut0226@gmail.com>
2024-10-24 18:11:41 +0900

    programmers java/js[hjy]

--- coding-test/src/hjy/프로그래머스/자릿수더하기/Solution.js
+++ coding-test/src/hjy/프로그래머스/자릿수더하기/Solution.js
@@ -1,13 +1,13 @@
 function solution(n)
 {
-     var answer = 0;
+     let answer = 0;
     // let str = str.split('');
     // for (let i = 0; i < str.length; i++) {
     //     answer += n[i]
     // }
     // return answer;
     return n.toString().split('').reduce((sum, num) => {
-      sum + parseInt(num);
-    },0)
+       parseInt(sum) + parseInt(num);
+    }, 0 )
 }
 console.log(solution(123));
--- coding-test/src/jjy/프로그래머스/평균구하기/solution.js
+++ coding-test/src/jjy/프로그래머스/평균구하기/solution.js
@@ -1,3 +1,5 @@
 function solution(arr) {
+
     return arr.reduce((a, b) => a + b, 0) / arr.length;
+
 }
