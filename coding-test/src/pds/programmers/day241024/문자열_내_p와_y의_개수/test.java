f65adc73066f700c1470f02df9222949b22742e3
Parkdongshin <rktks789@naver.com>
2024-10-24 17:01:25 +0900

    ãprogrammers 2problem[pds]

---
+++ coding-test/src/pds/programmers/day241024/문자열_내_p와_y의_개수/test.java
@@ -0,0 +1,23 @@
+//class Solution {
+//    boolean solution(String s) {
+//        boolean answer = true;
+//        int count = 0;
+//
+//        String[] num = new string[10];
+//        num = s.toLowerCase().split('');
+//
+//
+//        for(int i = 0; i < num.length; i++){
+//            for(int j = 0; j < num.length; j++){
+//                if(num[i].equlas(num[j])){
+//                    count++;
+//
+//                }
+//
+//            }
+//
+//        }
+//
+//        return answer;
+//    }
+//}
