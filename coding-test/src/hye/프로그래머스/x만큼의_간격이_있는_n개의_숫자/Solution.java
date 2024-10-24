4884f958cd51a5302bd82c220df5e49f41d26b25
solsol <le_fox.sol@hwang-yeeun-ui-MacBookAir.local>
2024-10-24 11:59:34 +0900

    4문제 완료[hye]

---
+++ coding-test/src/hye/프로그래머스/x만큼의_간격이_있는_n개의_숫자/Solution.java
@@ -0,0 +1,5 @@
+package hye.프로그래머스.x만큼의_간격이_있는_n개의_숫자;
+
+public class Solution {
+
+}
--- coding-test/src/hye/프로그래머스/자릿수_더하기/Solution.java
+++ coding-test/src/hye/프로그래머스/자릿수_더하기/Solution.java
@@ -1,15 +1,14 @@
-package hye.프로그래머스.평균_구하기;
+package hye.프로그래머스.자릿수_더하기;
 
 public class Solution {
     public int solution(int n) {
         int answer = 0;
         String str = String.valueOf(n);
         String[] arr = str.split("");
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             Integer num = Integer.parseInt(arr[i]);
             answer += num;
         }
         return answer;
     }
 }
-
