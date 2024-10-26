563f6ad73caa8b9ecffeee089e692b5924cfd94e
wns0901 <wnswns012@gmail.com>
2024-10-26 15:59:35 +0900

    29일 스터티 문제(완)[jjy]

--- coding-test/src/jjy/프로그래머스/day241029/정수_제곱근_판별/Solution.java
+++ coding-test/src/jjy/프로그래머스/day241029/정수_제곱근_판별/Solution.java
@@ -1,4 +1,7 @@
 package jjy.프로그래머스.day241029.정수_제곱근_판별;
 
 public class Solution {
+    public long solution(long n) {
+        return Math.sqrt(n) % 1 == 0 ? (long) Math.pow(Math.sqrt(n) + 1, 2) : -1L;
+    }
 }
