4b1fae4652dc53dbdd06b5f137cd908226a47144
solsol <le_fox.sol@hwang-yeeun-ui-MacBookAir.local>
2024-10-27 20:43:57 +0900

    3문제 완료[hye]

--- coding-test/src/hye/프로그래머스/day241029/서울에서_김서방_찾기/Solution.java
+++ coding-test/src/hye/프로그래머스/day241029/서울에서_김서방_찾기/Solution.java
@@ -1,4 +1,14 @@
 package hye.프로그래머스.day241029.서울에서_김서방_찾기;
 
 public class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int i = 0;
+        for(String e : seoul) {
+            if(e.matches("Kim")) {break;}
+            i++;
+        }
+        answer = "김서방은 " + i + "에 있다";
+        return answer;
+    }
 }
