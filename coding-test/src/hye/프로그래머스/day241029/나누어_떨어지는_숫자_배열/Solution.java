ae9cdd12d9e45948c683feaa47bdd9649e46f145
solsol <le_fox.sol@hwang-yeeun-ui-MacBookAir.local>
2024-10-29 14:59:22 +0900

    java 완료 [hye]

--- coding-test/src/hye/프로그래머스/day241029/나누어_떨어지는_숫자_배열/Solution.java
+++ coding-test/src/hye/프로그래머스/day241029/나누어_떨어지는_숫자_배열/Solution.java
@@ -1,4 +1,21 @@
 package hye.프로그래머스.day241029.나누어_떨어지는_숫자_배열;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int e : arr) {
+            if(e % divisor == 0) {
+                list.add(e);
+            }
+        }
+        if(list.isEmpty()) {
+            return new int[] {-1};
+        }
+        answer = list.stream().mapToInt(i -> i).toArray();
+        Arrays.sort(answer);
+        return answer;
+    }
 }
