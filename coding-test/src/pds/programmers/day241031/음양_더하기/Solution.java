eebd0d6dc2c3656b89f2f20e75860d603a0cfac3
Parkdongshin <rktks789@naver.com>
2024-10-31 18:09:06 +0900

    프로그래머스 평균구하기[pds] git add *; git add *; git commit -m "2문제 프로그래멋 pds";
    git add *;
    git commit -m "2문제프로그래밍pds"

--- coding-test/src/pds/programmers/day241031/음양_더하기/Solution.java
+++ coding-test/src/pds/programmers/day241031/음양_더하기/Solution.java
@@ -1,4 +1,18 @@
 package pds.programmers.day241031.음양_더하기;
 
-public class Solution {
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+
+        int result = 0;
+
+
+        for(int i = 0; i < absolutes.length; i++){
+            if(signs[i] == true){
+                result += absolutes[i];
+            }else{
+                result -= absolutes[i];
+            }
+        }
+        return result;
+    }
 }
