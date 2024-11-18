f4431de3d222b2878138b99dbd492e0a9c1c1aa3
le_fox.sol <solsolsol815@gmail.com>
2024-11-18 18:30:08 +0900

    1문제 수정[hye]

--- coding-test/src/hye/프로그래머스/day241118/약수의_개수와_덧셈/Solution.java
+++ coding-test/src/hye/프로그래머스/day241118/약수의_개수와_덧셈/Solution.java
@@ -1,9 +1,25 @@
 package hye.프로그래머스.day241118.약수의_개수와_덧셈;
 
 class Solution {
-    public boolean solution(String s) {
-        boolean answer = true;
-        answer = s.matches("^[0-9]{4}$|^[0-9]{6}$");
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int j = left; j <= right; j++) {
+            int count = 0;
+
+            for(int i = 1; i <= Math.sqrt(j); i++) {
+                if(j % i == 0) {
+                    count++;
+                    if(i != j / i) {
+                        count++;
+                    }
+                }
+            }
+            if(count % 2 == 0) {
+                answer += j;
+            } else {
+                answer -= j;
+            }
+        }
         return answer;
     }
 }
--- coding-test/src/hye/프로그래머스/day241118/약수의_개수와_덧셈/solution.js
+++ coding-test/src/hye/프로그래머스/day241118/약수의_개수와_덧셈/solution.js
@@ -1,5 +1,23 @@
 function solution() {
-    var answer = true;
-    answer = /^[0-9]{4}$|^[0-9]{6}$/.test(s);
+    var answer = 0;
+
+    for(let j = left; j <= right; j++) {
+        let count = 0;
+        for(let i = 1; i <= Math.sqrt(j); i++){
+            if(j % i == 0) {
+                count++;
+                if(i != j / i) {
+                    count++;
+                }
+            }
+        }
+        if(count % 2 == 0) {
+            answer += j;
+        } else {
+            answer -= j;
+        }
+    }
+
+
     return answer;
 }
