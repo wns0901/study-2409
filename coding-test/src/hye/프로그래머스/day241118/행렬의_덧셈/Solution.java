b0a5809a839be88307132c127bb7a22df5fa5de2
le_fox.sol <solsolsol815@gmail.com>
2024-11-18 18:51:14 +0900

    수정[hye]

--- coding-test/src/hye/프로그래머스/day241118/행렬의_덧셈/Solution.java
+++ coding-test/src/hye/프로그래머스/day241118/행렬의_덧셈/Solution.java
@@ -12,6 +12,17 @@ class Solution {
         return answer;
     }
 
+
+//    class Solution {
+//        public int[][] solution(int[][] arr1, int[][] arr2) {
+//            return Arrays.stream(arr1) // arr1의 각 행에 대해
+//                    .map(row -> Arrays.stream(row)  // 각 행의 요소에 대해
+//                            .map(i -> i + arr2[Arrays.asList(arr1).indexOf(row)][Arrays.asList(arr1[0]).indexOf(i)]) // 합산
+//                            .toArray())  // 결과를 다시 배열로 변환
+//                    .toArray(int[][]::new);  // 결과 배열로 변환
+//        }
+//    }
+
 }
 
 
