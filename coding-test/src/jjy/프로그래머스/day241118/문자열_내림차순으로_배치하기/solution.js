1883ea86ca95fd44fa14efb4bccb0bf01b54d451
jjy <wnswns012@gmai.com>
2024-11-18 18:04:50 +0900

    js만 품[jjy]

--- coding-test/src/jjy/프로그래머스/day241118/문자열_내림차순으로_배치하기/solution.js
+++ coding-test/src/jjy/프로그래머스/day241118/문자열_내림차순으로_배치하기/solution.js
@@ -1,2 +1,3 @@
-function solution() {
+function solution(s) {
+    return s.split('').sort((a,b) =>  b.charCodeAt(0) - a.charCodeAt(0)).join("");
 }
--- coding-test/src/jjy/프로그래머스/day241118/부족한_금액_계산하기/solution.js
+++ coding-test/src/jjy/프로그래머스/day241118/부족한_금액_계산하기/solution.js
@@ -1,2 +1,7 @@
-function solution() {
+function solution(price, money, count) {
+    let total = 0;
+    for (let index = 1; index <= count; index++) {
+        total += index * price;
+    }
+    return money - total > 0 ? 0 : (money - total) * -1;
 }
